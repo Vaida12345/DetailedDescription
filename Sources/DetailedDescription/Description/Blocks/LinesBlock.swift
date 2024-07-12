f03d279352507c7ccb4b5dbdd381cb7db2cddce8
Vaida <developer@vaidaf.net>
2024-07-12 17:17:12 +0800

    updates _LinesBlock

--- Sources/DetailedDescription/Description/Blocks/LinesBlock.swift
+++ Sources/DetailedDescription/Description/Blocks/LinesBlock.swift
@@ -6,29 +6,10 @@
 //
 
 
-public struct _LinesBlock<each T: DescriptionBlockProtocol>: DescriptionBlockProtocol {
+public struct _LinesBlock<each T: DescriptionBlockProtocol> {
     
     let lines: (repeat each T)
     
-    
-    public func _detailedWrite<Target>(
-        to target: inout Target,
-        trivia: [_Trivia]
-    ) where Target : TextOutputStream {
-        var needsToWriteNewLine = false
-        
-        for line in repeat each lines {
-            guard !(line is EmptyBlock) else { continue }
-            
-            if needsToWriteNewLine {
-                target.write("\n")
-                needsToWriteNewLine = false
-            }
-            
-            line._detailedWrite(to: &target, trivia: trivia)
-            needsToWriteNewLine = true
-        }
-    }
     
     init(lines: (repeat each T)) {
         self.lines = lines
